6b8aadad2f344c7f431f473f6e99b90d88c1460f
Nikolai-ip <petruchok02@mail.ru>
2021-10-25 21:29:37 +0700

    9

--- Script1.js
+++ Script1.js
@@ -1,7 +1,8 @@
 let k = 0;
 document.cookie = k;
 k++;
-document.getElementById("visits").nodeValue = k;
+Input = document.getElementById("visits").value;
+Input.value = k;
 document.cookie = "name=oeschger";
 document.cookie = "favorite_food=tripe";
 function alertCookie() {
