78bd2a7d601ca3092e26d7c8d442230bb481afe0
Nikolai-ip <petruchok02@mail.ru>
2021-10-27 15:01:01 +0700

    12

--- Script1.js
+++ Script1.js
@@ -1,12 +1,44 @@
-let k = 0;
+let name = document.getElementById("name");
+function signOn() {
+    console.log(name);
+    setCookie('name', name.value, { secure: true, 'max-age': 3600 });
+}
+console.log(getCookie('name'));
+if (getCookie('name') != undefined) {
+    document.getElementById("Hello").value = getCookie('name');   
+    document.getElementById("button_sign").style.display = "block";
+}
 
-k++;
-document.cookie = `${k}; expires=/; path=/`;
-console.log(k);
-Input = document.getElementById("visits");
-Input.value = k;
-document.cookie = "name=oeschger";
-document.cookie = "favorite_food=tripe";
-function alertCookie() {
-    alert(document.cookie);
+
+
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/',
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
 }
